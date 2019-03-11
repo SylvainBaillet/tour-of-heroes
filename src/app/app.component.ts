a31d9f95a9e9adf6b03970eda310f6ba7dbdb47f
SylvainBaillet <45996506+SylvainBaillet@users.noreply.github.com>
2019-03-11 16:31:28 +0100

    angular_app_component_11032019

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'tour-of-heroes';
+  title = 'Nassiiiiiiiiim';
+  heroes = [
+    'superman', 
+    'wonderwoman',
+    'Prettywoman'
+  ];
+  name = '';
+  heroToAdd = '';
+  //
+  addHero(){
+    // const heroName = 'Nassim';// l'orsqu'on declare un 'const' c'est quand notre élément sera constant et ne changera pas, sinon on utilise let
+    // this.heroes.push(heroName)// la j'ai juste tappé heroes et angular a ajouté le this automatiquement, push va permettre de creer un nouvel élément dans heroName
+    if(this.heroToAdd.length >0){//cette ecriture aurait fonctionné aussi: if(this.heroToAdd !== ''){}
+    // if (this.heroToAdd.trim().length > 0) { //cette autre ecriture, verifie avec .trim que l'input n'a pas été rempli par des espaces 
+      this.heroes.push(this.heroToAdd);
+      this.heroToAdd ='';
+      
+    }else{
+      alert('le nom du heros ne doit pas être vide')
+    }
+  }
 }
